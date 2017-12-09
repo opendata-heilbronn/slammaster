17ec70cce8096b70b410e1ba881c37a6853e7c3f
Patrick Hahn <patricksilashahn@gmx.net>
2017-12-09 15:25:50 +0100

    npm development start script

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
 
@@ -8,39 +8,39 @@ let win = null;
 
 app.on('ready', function () {
 
-  // Initialize the window to our specified dimensions
-  win = new BrowserWindow({ width: 1000, height: 600 });
-  win.setMenu(null);
-  // Specify entry point
+    // Initialize the window to our specified dimensions
+    win = new BrowserWindow({width: 1000, height: 600});
+    win.setMenu(null);
+    // Specify entry point
 
-  console.info(process.env.PACKAGE);
+    console.info(process.env.PACKAGE);
 
-  if (process.env.PACKAGE === 'true') {
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
-  } else {
-    win.loadURL(process.env.HOST);
-    win.webContents.openDevTools();
-  }
+    if (process.env.PACKAGE === 'true') {
+        win.loadURL(url.format({
+            pathname: path.join(__dirname, 'app', 'index.html'),
+            protocol: 'file:',
+            slashes: true
+        }));
+    } else {
+        win.loadURL(process.env.HOST);
+        win.webContents.openDevTools();
+    }
 
-  // Remove window once app is closed
-  win.on('closed', function () {
-    win = null;
-  });
+    // Remove window once app is closed
+    win.on('closed', function () {
+        win = null;
+    });
 
 });
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
+    if (win === null) {
+        createWindow();
+    }
 });
 
 app.on('window-all-closed', function () {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
